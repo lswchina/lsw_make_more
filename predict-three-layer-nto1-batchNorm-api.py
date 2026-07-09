d8e2d52051c75688f3107d904f2acce16294dabf
lswchina <lisuwan@smail.nju.edu.cn>
2026-07-09 09:51:59 +0200

    feat: copy from predict-three-layer-nto1-batchNorm.py

---
+++ predict-three-layer-nto1-batchNorm-api.py
@@ -0,0 +1,276 @@
+'''
+实现了一个三层神经网络。基于前n个字符预测下一个字符。
+前两层都分别包含一个线性变换以及一个激活函数。
+这两层的结尾位置，手搓了一个batch normalization。
+第三层是输出层，包含一个线性变换。
+token编码映射到一个五维空间。
+
+
+总结：
+'''
+
+
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from itertools import chain
+
+BATCH_SIZE = 20
+TIME_SIZE = 3
+HIDDEN_FEATURES = 50
+EMBED_SIZE = 5
+
+
+class myLinear(nn.Module):
+	def __init__(self, in_features, hidden_features, bias=True):
+		super().__init__()
+		self.W = nn.Parameter(
+			torch.randn(in_features, hidden_features)
+		)
+		if not bias:
+			self.b = None
+		else:
+			self.b = nn.Parameter(
+				torch.zeros(hidden_features)
+			)
+
+	
+	def forward(self, x):
+		# print("linear:", x.shape)
+		out = x @ self.W
+		if self.b is not None:
+			out = out + self.b
+		return out
+	
+class myTanh(nn.Module):
+	def __init__(self):
+		super().__init__()
+
+	def forward(self, x):
+		exp_2x = torch.exp(2 * x)
+		negative = (exp_2x - 1) / (exp_2x + 1)
+		exp_minus_2x = torch.exp(-2 * x)
+		positive = (1 - exp_minus_2x) / (1 + exp_minus_2x)
+		return torch.where(x >= 0, positive, negative)
+
+class myBatchNorm(nn.Module):
+	def __init__(self, hidden_features, momentum=0.1):
+		super().__init__()
+		self.lamda = nn.Parameter(
+			torch.ones(hidden_features)
+		)
+		self.beta = nn.Parameter(
+			torch.zeros(hidden_features)
+		)
+		self.register_buffer(
+			"running_mean",
+			torch.zeros(hidden_features)
+		)
+		self.register_buffer(
+			"running_var",
+			torch.ones(hidden_features)
+		)
+		self.momentum = momentum
+	# Error-1: register these parameters so that they will be remembered in the model
+	# Error-2: memontum is a hyper-paremater, not a tensor, not required to move to GPU, intialize it normally
+	# Error-3: lamda is initialized as ones, beta is initialized as zeros, running_var is initialized as zeros
+
+
+	def forward(self, x): # x: B, H
+		if self.training:
+			mean = torch.mean(x, dim=0) # H
+			var = torch.var(x, dim=0) # H
+			# Error-4: no keepdim! otherwise the adding of mean/var to running_mean/running_var will destroy the shape
+
+			with torch.no_grad():
+				self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * mean
+				self.running_var = (1 - self.momentum) * self.running_var + self.momentum * var
+
+		else:
+			mean = self.running_mean
+			var = self.running_var
+
+		return self.lamda * (x - mean) / torch.sqrt(var + 1e-5) + self.beta
+
+class myLayer(nn.Module):
+	def __init__(self, in_features, hidden_features):
+		super().__init__()
+		self.layer = nn.Sequential(
+			myLinear(in_features, hidden_features, bias=False),
+			myBatchNorm(hidden_features),
+			myTanh()
+		)
+		# Error-5: batch should be before tanh
+
+	def forward(self, x):
+		return self.layer(x)
+	
+class myNetwork(nn.Module):
+	def __init__(self, in_features, out_features):
+		super().__init__()
+		self.network = nn.Sequential(
+			myLayer(in_features, HIDDEN_FEATURES),
+			myLayer(HIDDEN_FEATURES, HIDDEN_FEATURES),
+			myLinear(HIDDEN_FEATURES, out_features)
+		)
+	
+	def forward(self, x):
+		return self.network(x)
+
+class myEmbedding(nn.Module):
+	def __init__(self, vocab_size, embed_size):
+		super().__init__()
+		self.embedding = nn.Parameter(
+			torch.randn(vocab_size, embed_size)
+		)
+	
+	def forward(self, x):
+		return self.embedding[x]
+
+
+def acquire_names():
+	name_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "names.txt")
+	names = open(name_file, "r", encoding="utf-8").read().split("\n")
+	return names
+
+def form_training_set(dataset): 
+	"""
+	three input characters to one output character
+	"""
+
+	X_train = []
+	Y_train = []
+	for name in dataset:
+		prefix = '$' * TIME_SIZE
+		append_name = prefix + name + '$'
+		for i in range(len(append_name) - TIME_SIZE):
+			X_train.append(append_name[i: i + TIME_SIZE])
+			Y_train.append(append_name[i + TIME_SIZE])
+			# print(f"{append_name[i: i + TIME_SIZE]} -> {append_name[i + TIME_SIZE]}")
+	return X_train, Y_train
+
+def gen_encoder_decoder(dataset):
+	tokens = set()
+	for name in dataset:
+		for t in name:
+			tokens.add(t)
+	stoi = {'$': 0}
+	itos = {0: '$'}
+	tokens = sorted(tokens)
+	for t, ind in zip(tokens, range(1, len(tokens) + 1)):
+		stoi[t] = ind
+		itos[ind] = t
+	return stoi, itos
+
+def encode(input_, output_, stoi):
+	x_label = torch.tensor([[stoi[ch] for ch in str_] for str_ in input_])
+	y_label = torch.tensor([stoi[o] for o in output_])
+	return x_label, y_label
+
+def generate(embedding, model, encoder, decoder):
+	x = '$' * TIME_SIZE
+	res = ""
+
+	while True:
+		x_label = torch.tensor([[encoder[ch] for ch in x]]) # 1, T
+		x_embedding = embedding(x_label) # 1, T, C
+		x_embedding = x_embedding.view(1, -1) # 1, T * C
+		logits = model(x_embedding) # 1, O
+		probs = F.softmax(logits, dim=1)
+		# print(probs)
+
+		# sample the next token based on the probs
+		# print(probs[0])
+		m = torch.distributions.Categorical(probs[0])
+		sample = m.sample().item()
+		# print(f"sample {sample}")
+
+		# decode the next character
+		y = decoder[sample]
+
+		# reach the end
+		if y == '$':
+			break
+
+		# append x
+		res += y
+		x = x[1:] + y
+	return res
+
+def main():
+
+	# load the dataset
+	names = acquire_names()
+	X_train, Y_train = form_training_set(names)
+	encoder, decoder = gen_encoder_decoder(names)
+	X_label, Y_label = encode(X_train, Y_train, encoder)
+
+	# initialization
+	vocab_size = len(encoder)
+	embedding = myEmbedding(vocab_size, EMBED_SIZE)
+	model = myNetwork(EMBED_SIZE * TIME_SIZE, vocab_size)
+	optimizer = torch.optim.AdamW(
+		chain(
+			embedding.parameters(),
+			model.parameters()
+		),
+		lr = 0.001
+		# Error-6: lr=0.01 is too large for Adamw
+	)
+
+
+	# train
+	model.train()
+	for i in range(10000):
+		# sample
+		idx = torch.randint(X_label.shape[0], (BATCH_SIZE, ))
+		X_sample = X_label[idx] # B, T
+		Y_sample = Y_label[idx] # B
+
+		# foward once
+		X_embedding = embedding(X_sample) # B, T, C
+		X_embedding = X_embedding.view(BATCH_SIZE, -1) # B, T * C
+		logits = model(X_embedding) # B, O
+
+		# calculate the loss and backward
+		loss = F.cross_entropy(logits, Y_sample)
+		loss.backward()
+		if i % 1000 == 0:
+			print(loss.item())
+
+		# update the parameters and set the gredients to zero
+		optimizer.step()
+		optimizer.zero_grad()
+
+	# generate
+	model.eval()
+	for _ in range(10):
+		res = generate(embedding, model, encoder, decoder)
+		print(res)
+
+main()
+
+# loss:
+# 9.33348274230957
+# 2.884723424911499
+# 2.751929998397827
+# 2.9838929176330566
+# 2.4973878860473633
+# 2.4243874549865723
+# 2.571622848510742
+# 2.4847114086151123
+# 1.936316728591919
+# 2.2253358364105225
+
+# prediction
+# xyavey
+# ana
+# jilce
+# lasslp
+# throlin
+# gevannah
+# cbelaala
+# mayo
+# dahi
+# anoo
